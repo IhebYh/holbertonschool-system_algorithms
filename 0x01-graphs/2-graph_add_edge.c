b75a2ff12b88912661646be9898862635538ca7f
Iheb Yahyaoui <iheb.yah@gmail.com>
2023-01-11 21:30:40 +0100

    graph add edge

--- 0x01-graphs/2-graph_add_edge.c
+++ 0x01-graphs/2-graph_add_edge.c
@@ -3,22 +3,37 @@
 /**
  * graph_add_edge - adds edge to graph
  * @graph: pointer to graph object
- * @src: name of source vertex
- * @dest: name of destination vertex
- * @type: type of edge UNI/BI
+ * @str: string value for new vertex
  * Return: 1 on success else 0
  */
-int graph_add_edge(graph_t *graph, const char *src, const char *dest, edge_type_t type)
+int graph_add_edge(graph_t *graph, const char *str)
 {
-	vertex_t *firstVertex = NULL, *secondVertex = NULL;
-	edge_t *firstEdge, *secondEdge;
+	vertex_t *new_vertex, *vertex;
 
-	for ()
-	/*
-	TODO:
-	- get the vertices
-	- check the edges
-	- alloc the edges and add the dest
-	- return 1 on success else return 0
-	*/
+	if (!graph || !str)
+		return (NULL);
+	vertex = graph->vertices;
+	while (vertex)
+	{
+		if (!strcmp(vertex->content, str))
+			return (NULL);
+		if (!vertex->next)
+			break;
+		vertex = vertex->next;
+	}
+	new_vertex = malloc(size(vertex_t));
+	if (!new_vertex)
+		return (NULL);
+	new_vertex->content = strdup(str);
+	if (!new_vertex->content)
+	{
+		free(new_vertex);
+		return (NULL);
+	}
+	new_vertex->index = graph->nb_vertices++;
+	if (vertex)
+		vertex->next = new_vertex;
+	else
+		graph->vertices = new_vertex;
+	return (new_vertex);
 }
